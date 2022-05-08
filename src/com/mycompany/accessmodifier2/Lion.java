45d8e571174fa3ef1f3136008dd12b4b08a2b2ea
Ranjan <skillsapphire@gmail.com>
2022-05-08 20:05:38 +0200

    test

--- src/com/mycompany/accessmodifier2/Lion.java
+++ src/com/mycompany/accessmodifier2/Lion.java
@@ -6,7 +6,7 @@ public class Lion extends Animal {
     public void display()
     {
         /*Animal animal = new Animal();
-        System.out.println(animal.canFly);*/
+        System.out.println(animal.canFly); test*/
         System.out.println(canFly);//protected members can be accessed outside the package in the child classes
     }
 
